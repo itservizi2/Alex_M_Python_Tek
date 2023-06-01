9b5f68f3e6dd70b2e01a58132d225d079c999cfd
itservizi2 <itservizi.helpdesk@gmail.com>
2023-06-01 11:52:57 +0300

    Test commit

---
+++ Hello.py
@@ -0,0 +1,96 @@
+name = "Alex"
+hello_string = "Salut " + name
+print(hello_string)
+
+
+# Declararea și atribuirea valorilor variabilelor
+name = "John"  # Atribuirea unei valori de tip șir de caractere
+age = 25  # Atribuirea unei valori de tip întreg
+is_student = True  # Atribuirea unei valori de tip boolean
+
+# Afișarea valorilor variabilelor
+print(name)
+print(age)
+print(is_student)
+
+nume = "Alice"
+varsta = "25"
+print("Nume: " + nume + ", Varsta: " + varsta)
+
+print("Python", "este", "grozav!")
+
+name = "John"  # Șir de caractere
+age = 25  # Număr întreg
+height = 1.75  # Număr real
+is_student = True  # Valoare booleană
+
+print(name)
+print(type(name)) # Ar trebui să fie str
+print(age)
+print(type(age)) # Ar trebui să fie int
+print(height)
+print(type(height)) # Ar trebui să fie float
+print(is_student)
+print(type(is_student)) # Ar trebui să fie bool
+name = "John"  # Șir de caractere
+age = 25  # Număr întreg
+height = 1.75  # Număr real
+is_student = True  # Valoare booleană
+
+# Declararea șirurilor de caractere folosind ghilimele simple
+string1 = 'Salut, lume!'
+string2 = 'Acesta este un șir de caractere.'
+
+# Declararea șirurilor de caractere folosind ghilimele duble
+string3 = "Programare în Python"
+string4 = "Îmi place să codific!"
+
+# Declararea șirurilor de caractere cu ghilimele în interior
+string5 = "El a spus, 'Salut!'"
+string6 = 'Ea a răspuns, "Bună ziua!"'
+
+string1 = "Salut "
+string2 = "lume"
+rezultat = string1 + string2
+print(rezultat)
+
+nume = "Alice"
+salut = "Salut, " + nume + "!"
+print(salut)
+
+mesaj = "Salut! " * 3
+print(mesaj)
+
+# Declara numere reale
+float_num1 = 3.14
+float_num2 = 2.5
+
+# Declara numere întregi
+int_num1 = 10
+int_num2 = -5
+
+x = 10  # număr întreg
+y = 3.14  # număr real
+
+rezultat = x + y  # Rezultatul va fi un număr real: 13.14
+
+a = 10
+b = 3
+rezultat = a // b  # Rezultatul este 3
+
+name = "Alice"
+age = 25
+message = f"Numele meu este {name} și am {age} de ani."
+print(message)
+
+name = "Alice"
+age = 25
+message = "Numele meu este {} și am {} de ani.".format(name, age)
+print(message)
+
+name = input("Introduceți numele dumneavoastră: ")
+print("Salut, " + name + "! Bine ați venit în program.")
+
+x=5
+y=10
+print(x+y)
